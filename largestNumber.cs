732a6a34ac9f0679ca26cfb6d9ef76740a2d9e6e
Binoy Narayanan <binoyzone@gmail.com>
2023-03-22 19:05:45 +0530

    Added largest number calculator class

--- largestNumber.cs
+++ largestNumber.cs
@@ -1,1 +1,15 @@
-Console.WriteLine("Finding largest numnber")
+using System;
+
+Console.WriteLine("Finding largest numnber");
+int [] k = {3,2,7,6,9,4,8};
+int largestNum = 0;
+
+foreach (int i in k)
+{
+	if(i > largestNum)
+	{
+		largestNum = i;
+	}
+}
+
+Console.WriteLine("Largest number in array is : " + largestNum);
